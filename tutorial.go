a8c91b071998bfae886e85a3b2f4c54775d0eb41
Kien Do <kienmarkdo@gmail.com>
2022-01-11 16:13:06 -0500

    first commit

---
+++ tutorial.go
@@ -0,0 +1,6 @@
+/*
+Learning Golang.
+
+Currently following Tech with Tim videos to learn the basic syntax of Golang.
+The goal is to learn the syntax quickly to get a head start in CSI2520 - Programming Paradigms
+*/
