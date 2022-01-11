c8951cbeeece463afe8c416fd8be4a296bf04d6e
Kien Do <kienmarkdo@gmail.com>
2022-01-11 16:49:32 -0500

    key/value data structure; map

--- tutorial.go
+++ tutorial.go
@@ -12,22 +12,17 @@ import (
 )
 
 func main() {
-	// empty array of int with a specified size
-	var a [5]int
-	a[2] = 7
+	// key value data structure
+	vertices := make(map[string]int)
 
-	// array of int with a non-specified size (slices) (size cannot be change after initialization)
-	arrSlice := []int{0, 1, 2}
-	// this will not work arrSlice[3] = 3
-	fmt.Println(arrSlice)
+	vertices["triangle"] = 3
+	vertices["square"] = 4
+	vertices["circle"] = 0
 
-	b := []int{5, 4, 3, 2, 1}
-	b = append(b, 13) // creates a new array, recopies it back into b
-	fmt.Println(b)
+	fmt.Println(vertices)
 
-	// This code below would not work as it is not a slice
-	// arrFixedLen := [2]string{"word1", "word2"}
-	// arrFixedLen = append(arrFixedLen, "word3")
-	// fmt.Println(arrFixedLen)
+	delete(vertices, "square") // see more map functions here https://yourbasic.org/golang/maps-explained/
+
+	fmt.Println(vertices)
 
 }
