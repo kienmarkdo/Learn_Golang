72d8845baea032e85d14ec34a2508b7ced70c870
Kien Do <kienmarkdo@gmail.com>
2022-01-11 16:15:42 -0500

    print hello world

--- tutorial.go
+++ tutorial.go
@@ -1,6 +1,14 @@
 /*
 Learning Golang.
 
-Currently following Tech with Tim videos to learn the basic syntax of Golang.
+Currently following Tech with Tim and Jake Wright videos to learn the basic syntax of Golang.
 The goal is to learn the syntax quickly to get a head start in CSI2520 - Programming Paradigms
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+}
