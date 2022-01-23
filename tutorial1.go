efd9f640fc9ec1942c6b6a69ce0103739671c61a
Kien Do <kienmarkdo@gmail.com>
2022-01-22 22:05:21 -0500

    different code and text file for q2

--- tutorial1.go
+++ tutorial1.go
@@ -11,8 +11,30 @@ func check(e error) {
 	}
 }
 
+// func main() {
+// 	dat, err := os.ReadFile("pays.txt")
+// 	check(err)
+// 	fmt.Print(string(dat))
+// }
+
 func main() {
-	dat, err := os.ReadFile("pays.txt")
-	check(err)
-	fmt.Print(string(dat))
+	file, err := os.Open("pays.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	var (
+		nom        string
+		population int
+	)
+
+	for {
+		n, err := fmt.Fscan(file, &nom, &population)
+
+		if err == nil {
+			fmt.Println(n, nom, population)
+		} else {
+			break
+		}
+	}
 }
