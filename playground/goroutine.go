c2cd7c3b54db436f445a5dd25b336610e9570830
Kien Do <kienmarkdo@gmail.com>
2022-02-07 19:02:22 -0500

    added get-around for sending/receiving in same function constraint

--- playground/goroutine.go
+++ playground/goroutine.go
@@ -8,9 +8,13 @@ import (
 
 func main() {
 
-	c := make(chan string)
-	c <- "hello" // this doesn't work because the program stops here to wait to receive info from the sender
+	c := make(chan string, 2) // this gets around the sending/receiving issue; but the channel size must be declared
+	c <- "hello"              // this doesn't work because the program stops here to wait to receive info from the sender
+	c <- "world"
 
 	msg := <-c
+	fmt.Println(msg)
+
+	msg = <-c
 	fmt.Println(msg)
 }
