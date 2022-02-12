fba42c39fd12f34bf47846066dd3ab6515eaf426
Kien Do <kienmarkdo@gmail.com>
2022-02-11 19:02:23 -0500

    added facts and rules examples

--- Logic Prolog/Playground/first.pl
+++ Logic Prolog/Playground/first.pl
@@ -1,4 +1,22 @@
-language(prolog).
-language(scheme).
-language(java).
-language(c).
+% These are for comments
+% Comments start with a %
+% Statements end with a .
+% Variables start with an upper case
+% Atoms start with a lower case
+
+% Facts
+man(adam).
+man(peter).
+man(paul).
+woman(marry).
+woman(eve).
+
+% Relations
+parent(adam,peter). % means adam is parent of peter
+parent(eve,peter).
+parent(adam,paul).
+parent(marry,paul).
+
+% Rules
+father(F,C) :- man(F),parent(F,C).
+mother(M,C) :- woman(M),parent(M,C).
