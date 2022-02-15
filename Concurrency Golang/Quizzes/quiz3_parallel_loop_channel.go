557303dff5197c3ae3a1aa959071ca30eb882106
Kien Do <kienmarkdo@gmail.com>
2022-02-15 14:51:28 -0500

    completed quiz3 using channels instead of wait groups

--- Concurrency Golang/Quizzes/quiz3_parallel_loop_channel.go
+++ Concurrency Golang/Quizzes/quiz3_parallel_loop_channel.go
@@ -1,31 +1,28 @@
+// version 2 en utilisant les channels au lieu de waitgroup
+
 package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 
 	x := []int{3, 1, 4, 1, 5, 9, 2, 6}
-
 	var y [8]int
+	c := make(chan int, len(x))
 
 	// boucle parallele
 
 	for i, v := range x {
 
 		go func(i int, v int) {
-
-			y[i] = calcul(v)
+			calcul(v, c)
 
 		}(i, v) // appel a la goroutine
 
+		y[i] = <-c
 	}
-
-	// ajouter synchronisation
-
-	time.Sleep(1 * time.Second)
 
 	fmt.Println(y)
 
@@ -33,8 +30,6 @@ func main() {
 
 // vous pouvez ajouter un channel a la liste des parametres
 
-func calcul(v int) int {
-
-	return 2*v*v*v + v*v
-
+func calcul(v int, c chan int) {
+	c <- 2*v*v*v + v*v
 }
