4483d5306518cc777fdc6526fdc382ccffe0bba1
Kien Do <kienmarkdo@gmail.com>
2022-03-11 19:58:03 -0500

    terminé quiz 2

--- Logic Prolog/Quizzes/quiz2.pl
+++ Logic Prolog/Quizzes/quiz2.pl
@@ -1,9 +1,15 @@
 % Question 1 (1 point) 
 % Le prédicat suivant:
 
-% gSeries(X,Y):-gSeries(X,0,Y).
-% gSeries(X,N,Y):-power(X,N,Y).
-% gSeries(X,N,Y):- N1 is N+1, power(X,N1,Y), gSeries(X,N1,Y).
+power( X, 1, X ).
+power( X, Y, Z ) :- Y > 1, Y1 is Y - 1,
+power( X, Y1, Z1 ), Z is X * Z1.
+
+
+gSeries(X,Y):-gSeries(X,0,Y).
+gSeries(X,N,Y):-power(X,N,Y).
+gSeries(X,N,Y):- N1 is N+1, gSeries(X,N1,Y).
+
 
 % devrait donner comme solutions, la suite des X à la N, pour N=0,1,2,3,4... Par exemple:
 
