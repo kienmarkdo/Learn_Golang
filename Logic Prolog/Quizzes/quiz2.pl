e1c1fa0eeee5e782c02a8aa3cfc85a1182943a76
Kien Do <kienmarkdo@gmail.com>
2022-03-11 20:04:30 -0500

    ajouté le cas où x^0 = 1

--- Logic Prolog/Quizzes/quiz2.pl
+++ Logic Prolog/Quizzes/quiz2.pl
@@ -1,15 +1,9 @@
 % Question 1 (1 point) 
 % Le prédicat suivant:
 
-power( X, 1, X ).
-power( X, Y, Z ) :- Y > 1, Y1 is Y - 1,
-power( X, Y1, Z1 ), Z is X * Z1.
-
-
-gSeries(X,Y):-gSeries(X,0,Y).
-gSeries(X,N,Y):-power(X,N,Y).
-gSeries(X,N,Y):- N1 is N+1, gSeries(X,N1,Y).
-
+% gSeries(X,Y):-gSeries(X,0,Y).
+% gSeries(X,N,Y):-power(X,N,Y).
+% gSeries(X,N,Y):- N1 is N+1, power(X,N1,Y), gSeries(X,N1,Y).
 
 % devrait donner comme solutions, la suite des X à la N, pour N=0,1,2,3,4... Par exemple:
 
@@ -24,3 +18,15 @@ gSeries(X,N,Y):- N1 is N+1, gSeries(X,N1,Y).
 % Malheureusement, le résultat obtenu n'est pas le bon.
 
 % Pouvez-vous corriger ce prédicat? Ne changer qu'une seule formule.
+
+% Réponse
+
+power( X, 0, 1 ). % pour x^0.
+power( X, 1, X ).
+power( X, Y, Z ) :- Y > 1, Y1 is Y - 1,
+power( X, Y1, Z1 ), Z is X * Z1.
+
+
+gSeries(X,Y):-gSeries(X,0,Y).
+gSeries(X,N,Y):-power(X,N,Y).
+gSeries(X,N,Y):- N1 is N+1, gSeries(X,N1,Y).
