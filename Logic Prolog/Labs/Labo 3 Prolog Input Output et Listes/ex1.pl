01b7c0d96c8e3dd496837cb924b5ea7c0c05c81a
Kien Do <kienmarkdo@gmail.com>
2022-03-13 20:58:07 -0400

    terminé labo 6 prolog

--- Logic Prolog/Labs/Labo 3 Prolog Input Output et Listes/ex1.pl
+++ Logic Prolog/Labs/Labo 3 Prolog Input Output et Listes/ex1.pl
@@ -11,20 +11,5 @@
 % will loop forever with the same number because N is N-1 fails
 % Free variable cannot be re-assigned in Prolog and will not work.
 
-% start :- repeat,
-% decompte2(N),
-% Y = end, !.
-
-% decompte2(1). % base case
-% decompte2(N) :- 
-%     writeln(N)
-%     N1 is N-1,
-%     decompte2(N1).
-
-% decompte2(1) :- !.
-% decompte2(N) :-
-%     NN is N - 1,
-%     decompte2(NN).
-
 decompte2(N) :- N < 0, !. % base case
 decompte2(N) :- writeln(N), N1 is N - 1, decompte2(N1).
