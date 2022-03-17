b7ae1e4423ae4cfacb5a1a6864a46be19cc7d67a
Kien Do <kienmarkdo@gmail.com>
2022-03-16 21:49:18 -0400

    RÉSOLU EX2.PL
    EN UTILISANT EX4.PL POUR M'AIDERgit add .

--- Logic Prolog/Labs/Labo 4 Manipulation de listes/ex2.pl
+++ Logic Prolog/Labs/Labo 4 Manipulation de listes/ex2.pl
@@ -19,13 +19,55 @@
 % oddEven([T|Q], L) :- oddEven(Q, LL), mod(T, 2) =:= 0, L is [even|LL].
 % oddEven([T|Q], L) :- oddEven(Q, LL), mod(T, 2) =:= 1, L is [odd|LL].
 
-insert(A, L, [A|L]). % insert element A in front of L
 
-oddEven([T|[]], [even]) :- mod(T, 2) =:= 0, !.
-oddEven([T|[]], [odd]) :- mod(T, 2) =:= 1.
 
-oddEven([T|Q], [even|Q]) :- mod(T, 2) =:= 0, !.
-oddEven([T|Q], [odd|Q]) :- mod(T, 2) =:= 1.
 
-oddEven([T|Q], L) :- oddEven(Q, LL), insert(LL, Q, L).
+% insert(A, L, [A|L]). % insert element A in front of L
 
+% oddEven([T|[]], [even]) :- mod(T, 2) =:= 0, !.
+% oddEven([T|[]], [odd]) :- mod(T, 2) =:= 1.
+
+% oddEven([T|Q], [even|Q]) :- mod(T, 2) =:= 0, !.
+% oddEven([T|Q], [odd|Q]) :- mod(T, 2) =:= 1.
+
+% oddEven([T|Q], L) :- oddEven(Q, LL), insert(LL, Q, L).
+
+
+% Working version submit START
+oddEven(L, R) :- oddEven(L, [], RR), notre_reverse(RR, R). % [] is the final list, X is the odd/even indicator
+oddEven([], AA, AA). % end case for when the input list L is empty
+oddEven([H|T], A, R) :-
+            mod(H, 2) =:= 1,
+            insert(odd, A, AA),
+            oddEven(T, AA, R).
+oddEven([H|T], A, R) :-
+            mod(H, 2) =:= 0,
+            insert(even, A, AA),
+            oddEven(T, AA, R).
+
+% Helper functions
+insert(A, L, [A|L]).
+
+renverser([],L,L) :- !.
+renverser([H|T],L,R) :- renverser(T,[H|L],R).
+
+notre_reverse(L,R) :- renverser(L,[],R).
+% working version submit END
+
+
+
+% working version START
+% oddEven(IN, OUT) :- find(IN, [], REV), reverse(REV,OUT).
+
+% even(X) :- 0 is mod(X, 2).
+% odd(X) :- 1 is mod(X, 2).
+
+% process(X,OLD, NEW) :- even(X), ! , insert(even,OLD,NEW).
+% process(X,OLD, NEW) :- insert(odd,OLD,NEW).
+
+% find([],X,X). %base case
+% find([F|L], LIST, OUT) :- process(F,LIST,L2), find(L,L2,OUT).
+
+%Helper function
+% insert(A,L,[A|L]).
+% working version END
