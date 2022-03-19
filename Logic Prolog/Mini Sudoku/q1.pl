a35158c0e956b507111956f458e3c2947fdce63c
Kien Do <kienmarkdo@gmail.com>
2022-03-19 00:59:53 -0400

    Ajouté Q1

---
+++ Logic Prolog/Mini Sudoku/q1.pl
@@ -0,0 +1,12 @@
+% Nom: Kien Do
+% Cours: CSI 2520 - Paradigms de programation Hiver 2022
+% Professeur: Robert Laganière
+% Devoir Prolog - Mini Sudoku
+
+% Question 1
+
+% Définir le prédicat different/1 qui est vrai lorsque tous les nombres d’une liste sont différents.
+% ?- different([1,3,6,4,8,0]).
+% yes
+% ?- different([1,3,6,4,1,8,0]).
+% no
