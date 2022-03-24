22983330f9d49ee56b33b3395d8e445afec45ffe
Kien Do <kienmarkdo@gmail.com>
2022-03-23 22:07:35 -0400

    Terminé quiz 1 scheme

---
+++ Scheme Functional/Quizzes/quiz1_arithmetic.scm
@@ -0,0 +1,11 @@
+#| Quiz 1
+
+Question 1 (1 point) 
+On veut calculer le résultat de 4*(3+7+2+8+1) à l'aide de fonctions Scheme. Malheureusement l'appel suivant ne fonctionne pas. Corriger l'erreur:
+
+(4 * (3 + 7 + 2 + 8 + 1))
+
+|#
+
+#lang scheme
+(* 4 (+ 3 7 2 8 1))
---
+++ Scheme Functional/Quizzes/quiz1_correct_the_error.scm
@@ -0,0 +1,13 @@
+#| Old quiz 1 
+
+Question 1 (1 point) 
+Saved
+On veut calculer le résultat de 4*(3+7) à l'aide de fonctions Scheme. Malheureusement l'appel suivant ne fonctionne pas. Corriger l'erreur:
+
+(4 * (3 + 7))
+
+|#
+
+
+#lang scheme
+(* 4 (+ 3 7))
